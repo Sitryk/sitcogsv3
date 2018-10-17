29b22fb457972263efa3211ca61cc8349a1a2aa1
James <jamesfitzy1937@gmail.com>
2018-10-18 04:24:42 +1300

    Create __init__.py

---
+++ scrims/__init__.py
@@ -0,0 +1,7 @@
+from redbot.core import data_manager
+from .cog import Scrims
+
+async def setup(bot):
+    cog = Scrims(bot)
+    data_manager.load_bundled_data(cog, __file__)
+    bot.add_cog(cog)
