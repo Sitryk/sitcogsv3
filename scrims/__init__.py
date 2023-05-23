1e1f79cffe9ad7023d2d95a2a7d6276ef3b0f01f
James Fitzpatrick <jamesfitzy1937@gmail.com>
2023-05-23 19:56:48 +1200

    Delete scrims directory
    No intent to fix

--- scrims/__init__.py
+++
@@ -1,7 +0,0 @@
-from redbot.core import data_manager
-from .scrims import Scrims
-
-async def setup(bot):
-    cog = Scrims(bot)
-    data_manager.load_bundled_data(cog, __file__)
-    bot.add_cog(cog)
